927ecf046ae4d7f2788960ed6eca6d365e6988ff
niazmohsameer <niazmohsameer@gmail.com>
2022-01-04 20:26:15 -0500

    Add links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { MailIcon } from "@heroicons/react/solid";
 
 const Home: NextPage = () => {
     return (
@@ -8,8 +9,8 @@ const Home: NextPage = () => {
         >
             <div
                 className="w-4/6 max-w-md m-0 flex flex-col justify-center items-center rounded-md
-                          shadow-xl backdrop-brightness-150 cursor-pointer transition-all
-                          hover:shadow-2xl hover:backdrop-brightness-200 hover:-translate-y-2
+                          shadow-xl backdrop-brightness-150 transition-all hover:shadow-2xl
+                          hover:backdrop-brightness-200 hover:-translate-y-2
                           dark:text-white sm:flex-row"
             >
                 <div className="w-full p-4 flex justify-center items-center">
@@ -21,8 +22,21 @@ const Home: NextPage = () => {
                     />
                 </div>
                 <div className="w-full p-4 flex flex-col justify-center">
-                    <h1 className="text-2xl font-bold">Sameer M. Niaz</h1>
-                    <h2 className="text-lg">Software Developer</h2>
+                    <div>
+                        <h1 className="text-2xl font-bold">Sameer M. Niaz</h1>
+                        <h2 className="text-lg">Software Developer</h2>
+                    </div>
+                    <div className="mt-1 flex flex-row">
+                        <a href="mailto:sam@sameerniaz.com">
+                            <MailIcon className="h-6 mr-2 cursor-pointer opacity-70 hover:opacity-100" />
+                        </a>
+                        <a
+                            className="opacity-70 hover:opacity-100"
+                            href="github.com/niazmsameer"
+                        >
+                            GitHub
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
