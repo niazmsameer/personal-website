7107a3db4e5733a659e5f7ab65b0e16053fd097c
niazmohsameer <niazmohsameer@gmail.com>
2022-01-04 19:07:47 -0500

    Add portrait

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
     return (
-        <div className="h-screen w-screen mx-auto flex justify-center items-center bg-slate-400 dark:bg-slate-900 transition-all">
+        <div
+            className="h-screen w-screen mx-auto flex justify-center items-center
+                     bg-slate-400 dark:bg-slate-900 transition-all"
+        >
             <div
                 className="container h-32 w-64 flex justify-center items-center rounded-md
                           shadow-xl backdrop-brightness-150 cursor-pointer transition-all
